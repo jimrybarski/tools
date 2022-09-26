381d84db4b3c32cb0a57fcfb905d8ae4684046b8
Jim Rybarski <jim@rybarski.com>
2022-09-26 13:02:27 -0500

    recursive rustfmt alias

--- dotfiles/.config/zsh/rust.zsh
+++ dotfiles/.config/zsh/rust.zsh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 
 alias cbr='cargo build --release'
+alias cc='cargo check'
 alias ct='cargo test'
 alias cba='cargo test && cargo build --release && cargo clippy'
+# recursively format all Rust source files
+alias fmt="fd '\.rs$' -x rustfmt"
