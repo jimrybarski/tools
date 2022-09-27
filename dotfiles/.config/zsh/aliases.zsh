3fed8af6b5923ce01898df9b5d42ba7a4d09e8a0
Jim Rybarski <jim@rybarski.com>
2022-09-27 12:09:45 -0500

    add htop alias, jjjenes cd alias

--- dotfiles/.config/zsh/aliases.zsh
+++ dotfiles/.config/zsh/aliases.zsh
@@ -7,6 +7,7 @@ alias daysuntil="dateutils.ddiff -f '%Y years %d days' today"
 # system management
 alias plug='vim +silent +"PackerInstall" +qall && vim +silent +"PackerClean" +qall'
 alias upgrade='sudo apt update && sudo apt upgrade -y && sudo apt-get autoremove'
+alias hur="htop -u $USER"
 
 # CLI
 alias ll="exa -al --header --time-style long-iso"
@@ -14,3 +15,4 @@ alias ls="exa"
 alias mv="mv -i"  # always prompt if overwriting a file
 alias v="nvim"
 alias randpass="openssl rand -base64 32"
+alias jj="cd $WORK/jjjenes"
