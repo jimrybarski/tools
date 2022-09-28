8be70036702877ef8e1058af286560625faf4a2b
Jim Rybarski <jim@rybarski.com>
2022-09-28 15:04:10 -0500

    add local wilkecomp config

--- dotfiles/.config/zsh/local/wilkcomp02.ccbb.utexas.edu.zsh
+++ dotfiles/.config/zsh/local/wilkcomp02.ccbb.utexas.edu.zsh
@@ -1,1 +1,19 @@
-wilkcomp01.ccbb.utexas.edu.zsh
+WORK=/stor/work/Wilke/rybarskj
+CURRENT_PROJECT=final-mempads
+OUTPUT=$WORK/$CURRENT_PROJECT/output
+SRC=$WORK/$CURRENT_PROJECT/src
+LIB=$SRC/libmempads
+DATA=$WORK/$CURRENT_PROJECT/data
+SCRATCH=$WORK/$CURRENT_PROJECT/scratch
+PLOTS=$OUTPUT/plots
+
+
+alias src="cd $SRC"
+alias out="cd $OUTPUT"
+alias lib="cd $LIB"
+alias data="cd $DATA"
+alias plots="cd $PLOTS"
+alias s="cd $SCRATCH"
+
+lib
+clear
