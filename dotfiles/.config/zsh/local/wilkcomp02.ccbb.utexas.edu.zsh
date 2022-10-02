1368d19071fc1cff7cada53ed39d2a18173dd5ab
Jim Rybarski <jim@rybarski.com>
2022-10-02 11:26:16 -0500

    add analysis directory

--- dotfiles/.config/zsh/local/wilkcomp02.ccbb.utexas.edu.zsh
+++ dotfiles/.config/zsh/local/wilkcomp02.ccbb.utexas.edu.zsh
@@ -4,11 +4,13 @@ OUTPUT=$WORK/$CURRENT_PROJECT/output
 SRC=$WORK/$CURRENT_PROJECT/src
 LIB=$SRC/libmempads
 DATA=$WORK/$CURRENT_PROJECT/data
+ANALYSIS=$WORK/$CURRENT_PROJECT/analysis
 SCRATCH=$WORK/$CURRENT_PROJECT/scratch
 PLOTS=$OUTPUT/plots
 
 
 alias src="cd $SRC"
+alias an="cd $ANALYSIS"
 alias out="cd $OUTPUT"
 alias lib="cd $LIB"
 alias data="cd $DATA"
