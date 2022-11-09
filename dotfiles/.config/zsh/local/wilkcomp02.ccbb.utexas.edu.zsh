29b9258d0bcf558fb889976a630dcd667d3f0fd5
Jim Rybarski <jim@rybarski.com>
2022-11-09 10:11:17 -0600

    new wilkecomp aliases/shortcuts

--- dotfiles/.config/zsh/local/wilkcomp02.ccbb.utexas.edu.zsh
+++ dotfiles/.config/zsh/local/wilkcomp02.ccbb.utexas.edu.zsh
@@ -4,10 +4,12 @@ OUTPUT=$WORK/$CURRENT_PROJECT/output
 SRC=$WORK/$CURRENT_PROJECT/src
 LIB=$SRC/libmempads
 DATA=$WORK/$CURRENT_PROJECT/data
+FIGURES=$WORK/$CURRENT_PROJECT/figures
+REPORTS=$FIGURES/reports
 FIGURE_DATA=$WORK/$CURRENT_PROJECT/figure-data
 ANALYSIS=$WORK/$CURRENT_PROJECT/analysis
 SCRATCH=$WORK/$CURRENT_PROJECT/scratch
-PLOTS=$OUTPUT/plot
+PLOTS=$WORK/$CURRENT_PROJECT/plot
 
 
 alias src="cd $SRC"
@@ -16,8 +18,12 @@ alias out="cd $OUTPUT"
 alias lib="cd $LIB"
 alias data="cd $DATA"
 alias fda="cd $FIGURE_DATA"
+alias fig="cd $FIGURES"
 alias plot="cd $PLOTS"
 alias s="cd $SCRATCH"
+alias rep="cd $REPORTS"
+
+alias pandoc="$HOME/.cabal/bin/pandoc"
 
 lib
 clear
--- dotfiles/.config/zsh/science.zsh
+++ dotfiles/.config/zsh/science.zsh
@@ -5,3 +5,7 @@ alias len="$HOME/code/seqtools-cli/target/release/seqtools-cli len"
 alias rev="$HOME/code/seqtools-cli/target/release/seqtools-cli r"
 alias rc="$HOME/code/seqtools-cli/target/release/seqtools-cli rc"
 alias gc="$HOME/code/seqtools-cli/target/release/seqtools-cli gc"
+
+function pfam () {
+    rg $1 $DATA/pfam/all-domains.ssv
+}
