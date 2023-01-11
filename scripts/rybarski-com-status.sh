d0a51721e980dbdb2926ab045c8b9a3a51d4d444
Jim Rybarski <jim@rybarski.com>
2023-01-11 12:45:11 -0600

    fix broken script

--- scripts/rybarski-com-status.sh
+++ scripts/rybarski-com-status.sh
@@ -1,5 +1,8 @@
+#! /usr/bin/env bash
 sleep 30  # wait a reasonable amount of time to ensure our internet connection is established. technically a race condition, but meh
-result=$(curl -I https://rybarski.com/ 2> /dev/null | rg -q "200 OK")
-if [[ $? -ne "0" ]]; then
+
+output="$(curl -I https://www.rybarski.com 2>/dev/null | rg "200 OK")" 
+
+if [[ -z "$output" ]]; then
     echo "rybarski.com down!"
 fi
