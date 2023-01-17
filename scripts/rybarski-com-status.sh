431c982b02d6554d6a9f9e8a76f0c911d383875d
Jim Rybarski <jim@rybarski.com>
2023-01-17 12:43:03 -0600

    don't call bash directly, fix another website upstatus bug

--- scripts/rybarski-com-status.sh
+++ scripts/rybarski-com-status.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 sleep 30  # wait a reasonable amount of time to ensure our internet connection is established. technically a race condition, but meh
 
-output="$(curl -I https://www.rybarski.com 2>/dev/null | rg "200 OK")" 
+output="$(curl -I https://www.rybarski.com 2>/dev/null | grep "200 OK")" 
 
 if [[ -z "$output" ]]; then
     echo "rybarski.com down!"
