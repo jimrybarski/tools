45cfa9b2ca3a941d8c140e37e53f3cba0b112422
Jim Rybarski <jim@rybarski.com>
2023-01-17 18:11:14 -0600

    handle password changes

--- scripts/are-repos-uptodate.sh
+++ scripts/are-repos-uptodate.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+# See if our dotfiles/scripts need to be synced
 pushd $HOME/tools > /dev/null
 dotfiles_count=$(git status -s | wc -l)
-popd > /dev/null
-pushd $HOME/.password-store > /dev/null
-password_count=$(git status | grep -P '(ahead)|(behind)')
 popd > /dev/null
 if [[ $dotfiles_count -gt "0" ]]; then
     echo "uncommitted changes in tools repo"
 fi
+
+# See if we have any passwords to sync
+pushd $HOME/.password-store > /dev/null
+git fetch > /dev/null
+password_count=$(git status | grep -P '(ahead)|(behind)')
+popd > /dev/null
 if [[ -n $password_count ]]; then
     echo "uncommitted password change"
 fi
