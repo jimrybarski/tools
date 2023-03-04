d14c21859730b8f1d15d0bd58bf253ab214f9ba7
Jim Rybarski <jim@rybarski.com>
2023-03-04 14:37:10 -0600

    fix ytdl installer

--- installation/terminal/yt-dlp.sh
+++ installation/terminal/yt-dlp.sh
@@ -8,5 +8,5 @@ pushd $HOME/.local
 python3 -m venv ytdlpenv
 ytdlpenv/bin/python3 -m pip install wheel
 ytdlpenv/bin/python3 -m pip install yt-dlp
-ln -s $HOME/.local/ytdlpenv/bin/yt-dlp $HOME/.local/yt-dlp
+ln -s $HOME/.local/ytdlpenv/bin/yt-dlp $HOME/.local/bin/yt-dlp
 popd
