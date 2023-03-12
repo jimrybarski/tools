8901afe6a77ec936f76850b2baa8edc477dc6946
Jim Rybarski <jim@rybarski.com>
2023-03-12 10:21:45 -0500

    shellcheck fixes

--- installation/terminal/jupyter.sh
+++ installation/terminal/jupyter.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 # Creates a Python virtualenv with Jupyter and a bunch of math, biology, and visualization packages installed
  
-mkdir -p $HOME/.local
-pushd $HOME/.local 2>&1 >/dev/null
+if [[ -e "$HOME"/.local/jupyterenv ]]; then
+    exit 0
+fi
+
+mkdir -p "$HOME"/.local/bin
+
+pushd "$HOME"/.local >/dev/null 2>&1 || exit 1
 python3 -m venv jupyterenv
 jupyterenv/bin/pip install wheel
 jupyterenv/bin/pip install numpy scipy scikit-learn matplotlib biopython more-itertools 'seaborn[stats]' statsmodels jupyterlab pandas ipython jellyfish
-ln -s $HOME/.local/jupyterenv/bin/jupyterlab $HOME/.local/bin/
-popd 2>&1 >/dev/null
+ln -s "$HOME"/.local/jupyterenv/bin/jupyterlab "$HOME"/.local/bin/
+popd >/dev/null 2>&1 || exit 1
--- installation/terminal/vim.sh
+++ installation/terminal/vim.sh
@@ -1,7 +1,12 @@
+#!/usr/bin/env bash
+
 # install packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+mkdir -p "$HOME"/.local
+
 # install neovim
 git clone https://github.com/neovim/neovim.git
-pushd neovim 2>&1 > /dev/null
-make -j8 CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=$HOME/.local install
+pushd neovim >/dev/null 2>&1 || exit 1
+make -j8 CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX="$HOME"/.local install
+popd >/dev/null 2>&1 || exit 1
