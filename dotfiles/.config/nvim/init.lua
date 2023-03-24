9089ed6612de2020920479d7eb9ce1998d1b1304
Jim Rybarski <jim@rybarski.com>
2023-03-24 11:16:03 -0500

    migrate to new version of italic config options

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -57,7 +57,10 @@ require("gruvbox").setup({
   undercurl = true,
   underline = true,
   bold = true,
-  italic = false,
+  italic = {
+      strings = true,
+      comments = true,
+  },
   strikethrough = true,
   invert_selection = false,
   invert_signs = false,
@@ -65,10 +68,6 @@ require("gruvbox").setup({
   invert_intend_guides = false,
   inverse = true, -- invert background for search, diffs, statuslines and errors
   contrast = "soft", -- can be "hard", "soft" or empty string
-  overrides = {
-      Comment = { italic = true },
-      String = { italic = true },
-  },
   dim_inactive = false,
   transparent_mode = false,
 })
