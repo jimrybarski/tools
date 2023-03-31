e157e065c6b54c44a007e4888f972815c81fb4db
Jim Rybarski <jim@rybarski.com>
2023-03-31 16:56:00 -0500

    install docker and let a nonroot user interact with it

---
+++ installation/terminal/docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+has_docker=$(which docker)
+
+if [[ -z "$has_docker" ]]; then
+    sudo apt install docker.io 
+fi
+
+in_docker_group=$(groups | grep docker | wc -l)
+
+if [[ "$in_docker_group" -eq "0" ]]; then
+    sudo usermod -aG docker "$USER"
+    >&2 echo "You need to log out and in again before you can use docker."
+fi
