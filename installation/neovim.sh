ece08880666074d5c29353c83d98076717a7dd44
Jim Rybarski <jim@rybarski.com>
2023-06-09 09:35:38 -0400

    revert new config and fix old bugs

--- installation/neovim.sh
+++ installation/neovim.sh
@@ -9,7 +9,7 @@ if [[ ! -e "$HOME"/.local/neovim ]]; then
 fi
 
 pushd "$HOME"/.local/neovim
-make CMAKE_INSTALL_PREFIX=$HOME/.local/nvim  CMAKE_BUILD_TYPE=Release install
+make CMAKE_INSTALL_PREFIX="$HOME"/.local/nvim  CMAKE_BUILD_TYPE=Release install
 popd
 rm "$HOME"/.local/bin/nvim
 ln -s "$HOME"/.local/neovim/build/bin/nvim "$HOME"/.local/bin/
